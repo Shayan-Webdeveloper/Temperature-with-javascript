77016a90fa2bf596569b14d76a0a1716f1d5ce0a
Shayan-Webdeveloper <shayandec2@gmail.com>
2024-12-08 21:17:27 +0500

    Task Done

---
+++ Javascript Assignment 2.js
@@ -0,0 +1,16 @@
+var T = prompt("Enter temperature")
+if(T > 40){
+     document.write("It is too hot outside")
+}
+else if(T > 30){
+     document.write("The weather today is normal")
+}
+else if(T > 20){
+     document.write("Todays weather is cool")
+}
+else if(T > 10){
+     document.write("OMG! Todays weather is so cool")
+}
+else{
+     document.write("Please enter the write value")
+}
